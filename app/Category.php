a5af919552127693bd233a4dffe033071661f7da
gabrivava <gabriele.vavassori4@gmail.com>
2021-07-26 16:20:15 +0200

    definita relazione lato category

--- app/Category.php
+++ app/Category.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     // associo il modello Post.
-    public function post()
+    /**
+     * Get all of the posts for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
     {
-        return $this->hasMany('App\Post');
+        return $this->hasMany(Post::class);
     }
 }
