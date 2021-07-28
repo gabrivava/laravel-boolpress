4689b5479a3b80ca9f30c3ab9305e0438fe64957
gabrivava <gabriele.vavassori4@gmail.com>
2021-07-28 15:00:32 +0200

    creato modello Tag, -msrc e collegato modelli post e tag con belongsto

--- app/Post.php
+++ app/Post.php
@@ -18,4 +18,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App/Tag');
+    }
 }
